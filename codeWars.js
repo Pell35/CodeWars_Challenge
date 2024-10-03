872e627680004b435e54f87b25b19ae14c5daf5c
Pj Elliott <pjelliott11@gmail.com>
2024-10-03 15:53:27 -0500

    Completed my first "Kata" on Codewars, creating a function to combine names

---
+++ codeWars.js
@@ -0,0 +1,27 @@
+//Combine strings function
+//
+//  Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+//
+//  Example:
+//
+//  With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+function combineNames(firstName, lasName){
+return firstName +" " + lasName
+}
+
+console.log(combineNames("John", "Cena"));
+
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+  It should remove all values from list a, which are present in list b keeping their order.
+
+  arrayDiff([1,2],[1]) == [2]
+
+  If a value is present in b, all of its occurrences must be removed from the other:
+
+  arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+let newList = [];
+function arrayLoop(list1,list2){
+for (let i =0, )
+}
